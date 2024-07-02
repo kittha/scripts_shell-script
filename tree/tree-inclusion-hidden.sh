c08596e6ca7a4103c668faa9aa7e35b05d40e8af
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-07-03 06:34:38 +0700

    add print tree script

---
+++ tree/tree-inclusion-hidden.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Enable the inclusion of hidden files in the list
+shopt -s dotglob
+
+print_tree() {
+    local directory=$1
+    local prefix=$2
+    local files=("$directory"/*)
+    local last_index=$((${#files[@]} - 1))
+
+    for index in "${!files[@]}"; do
+        local file="${files[$index]}"
+        local base_file=$(basename "$file")
+
+        if [ "$index" -eq "$last_index" ]; then
+            echo "${prefix}└── ${base_file}"
+            new_prefix="${prefix}    "
+        else
+            echo "${prefix}├── ${base_file}"
+            new_prefix="${prefix}│   "
+        fi
+
+        if [ -d "$file" ]; then
+            print_tree "$file" "$new_prefix"
+        fi
+    done
+}
+
+root_directory="."
+
+print_tree "$root_directory" ""
+
+# DON'T DELETE THIS LINE
+shopt -u dotglob
